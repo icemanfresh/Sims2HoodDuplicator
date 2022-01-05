88f98da82dd294c8fc2f4d22febf9944bec76070
Aaron Ong <icmnfrsh@gmail.com>
2022-01-05 11:45:13 -0600

    Added neighborhood image preview

--- Sims2HoodDuplicator/MainForm.Designer.cs
+++ Sims2HoodDuplicator/MainForm.Designer.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.IO;
 
 namespace Sims2HoodDuplicator
 {
@@ -43,9 +42,11 @@ namespace Sims2HoodDuplicator
         {
             this.NeighborhoodDropdown = new System.Windows.Forms.ComboBox();
             this.DuplicateButton = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.MainPanel = new System.Windows.Forms.Panel();
+            this.NeighborhoodImageBox = new System.Windows.Forms.PictureBox();
             this.CopyProgressBar = new System.Windows.Forms.ProgressBar();
-            this.panel1.SuspendLayout();
+            this.MainPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.NeighborhoodImageBox)).BeginInit();
             this.SuspendLayout();
             // 
             // NeighborhoodDropdown
@@ -54,17 +55,18 @@ namespace Sims2HoodDuplicator
             | System.Windows.Forms.AnchorStyles.Right)));
             this.NeighborhoodDropdown.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.NeighborhoodDropdown.FormattingEnabled = true;
-            this.NeighborhoodDropdown.Location = new System.Drawing.Point(12, 12);
+            this.NeighborhoodDropdown.Location = new System.Drawing.Point(10, 182);
             this.NeighborhoodDropdown.Name = "NeighborhoodDropdown";
             this.NeighborhoodDropdown.Size = new System.Drawing.Size(402, 21);
             this.NeighborhoodDropdown.TabIndex = 0;
+            this.NeighborhoodDropdown.SelectedIndexChanged += new System.EventHandler(this.NeighborhoodDropdown_SelectedIndexChanged);
             // 
             // DuplicateButton
             // 
             this.DuplicateButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.DuplicateButton.AutoSize = true;
             this.DuplicateButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.DuplicateButton.Location = new System.Drawing.Point(352, 57);
+            this.DuplicateButton.Location = new System.Drawing.Point(350, 227);
             this.DuplicateButton.Name = "DuplicateButton";
             this.DuplicateButton.Size = new System.Drawing.Size(62, 23);
             this.DuplicateButton.TabIndex = 1;
@@ -72,22 +74,33 @@ namespace Sims2HoodDuplicator
             this.DuplicateButton.UseVisualStyleBackColor = true;
             this.DuplicateButton.Click += new System.EventHandler(this.DuplicateButton_Click);
             // 
-            // panel1
+            // MainPanel
             // 
-            this.panel1.Controls.Add(this.CopyProgressBar);
-            this.panel1.Controls.Add(this.NeighborhoodDropdown);
-            this.panel1.Controls.Add(this.DuplicateButton);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(424, 92);
-            this.panel1.TabIndex = 2;
+            this.MainPanel.Controls.Add(this.NeighborhoodImageBox);
+            this.MainPanel.Controls.Add(this.CopyProgressBar);
+            this.MainPanel.Controls.Add(this.NeighborhoodDropdown);
+            this.MainPanel.Controls.Add(this.DuplicateButton);
+            this.MainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainPanel.Location = new System.Drawing.Point(0, 0);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(424, 262);
+            this.MainPanel.TabIndex = 2;
+            // 
+            // NeighborhoodImageBox
+            // 
+            this.NeighborhoodImageBox.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.NeighborhoodImageBox.Location = new System.Drawing.Point(112, 16);
+            this.NeighborhoodImageBox.Name = "NeighborhoodImageBox";
+            this.NeighborhoodImageBox.Size = new System.Drawing.Size(200, 150);
+            this.NeighborhoodImageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.NeighborhoodImageBox.TabIndex = 3;
+            this.NeighborhoodImageBox.TabStop = false;
             // 
             // CopyProgressBar
             // 
             this.CopyProgressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.CopyProgressBar.Location = new System.Drawing.Point(12, 39);
+            this.CopyProgressBar.Location = new System.Drawing.Point(10, 209);
             this.CopyProgressBar.Name = "CopyProgressBar";
             this.CopyProgressBar.Size = new System.Drawing.Size(402, 12);
             this.CopyProgressBar.TabIndex = 2;
@@ -96,80 +109,19 @@ namespace Sims2HoodDuplicator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(424, 92);
-            this.Controls.Add(this.panel1);
-            this.MinimumSize = new System.Drawing.Size(440, 131);
+            this.ClientSize = new System.Drawing.Size(424, 262);
+            this.Controls.Add(this.MainPanel);
+            this.MinimumSize = new System.Drawing.Size(440, 301);
             this.Name = "MainForm";
             this.Text = "The Sims 2 Hood Duplicator";
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            this.MainPanel.ResumeLayout(false);
+            this.MainPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.NeighborhoodImageBox)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private void LocalizeUI()
-        {
-            Text = Strings.Program_Name;
-            DuplicateButton.Text = Strings.Duplicate;
-        }
-
-        private void PopulateNeighborhoodDropdown()
-        {
-            NeighborhoodDropdown.DisplayMember = "Name";
-            NeighborhoodDropdown.ValueMember = "Directory";
-            foreach (string pack in _packs)
-            {
-                string neighborhoodTemplatesDirectory = Functions.GetNeighborhoodTemplatesDirectory(pack);
-                if (neighborhoodTemplatesDirectory != null)
-                {
-                    string[] dirs = Directory.GetDirectories(neighborhoodTemplatesDirectory);
-                    foreach (string dir in dirs)
-                    {
-                        string folderName = Path.GetFileName(dir);
-                        if (_folderNeighborhoodNameMappings.ContainsKey(folderName))
-                        {
-                            NeighborhoodDropdown.Items.Add(new Neighborhood(_folderNeighborhoodNameMappings[folderName], dir));
-                        }
-                    }
-                }
-            }
-            NeighborhoodDropdown.SelectedIndex = 0;
-        }
-
-        private System.Windows.Forms.ComboBox NeighborhoodDropdown;
-        private System.Windows.Forms.Button DuplicateButton;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.ProgressBar CopyProgressBar;
-
-        public class Neighborhood
-        {
-            private string myName;
-            private string myDirectory;
-
-            public Neighborhood(string strName, string strDirectory)
-            {
-                this.myName = strName;
-                this.myDirectory = strDirectory;
-            }
-
-            public string Name
-            {
-                get
-                {
-                    return myName;
-                }
-            }
-
-            public string Directory
-            {
-                get
-                {
-                    return myDirectory;
-                }
-            }
-        }
     }
 }
 
--- Sims2HoodDuplicator/MainForm.cs
+++ Sims2HoodDuplicator/MainForm.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Sims2HoodDuplicator
 {
@@ -15,6 +16,85 @@ namespace Sims2HoodDuplicator
             InitializeComponent();
             LocalizeUI();
             PopulateNeighborhoodDropdown();
+        }
+
+        private void LocalizeUI()
+        {
+            Text = Strings.Program_Name;
+            DuplicateButton.Text = Strings.Duplicate;
+        }
+
+        private void PopulateNeighborhoodDropdown()
+        {
+            NeighborhoodDropdown.DisplayMember = "Name";
+            NeighborhoodDropdown.ValueMember = "Directory";
+            foreach (string pack in _packs)
+            {
+                string neighborhoodTemplatesDirectory = Functions.GetNeighborhoodTemplatesDirectory(pack);
+                if (neighborhoodTemplatesDirectory != null)
+                {
+                    string[] dirs = Directory.GetDirectories(neighborhoodTemplatesDirectory);
+                    foreach (string dir in dirs)
+                    {
+                        string folderName = Path.GetFileName(dir);
+                        if (_folderNeighborhoodNameMappings.ContainsKey(folderName))
+                        {
+                            NeighborhoodDropdown.Items.Add(new Neighborhood(_folderNeighborhoodNameMappings[folderName], dir));
+                        }
+                    }
+                }
+            }
+            NeighborhoodDropdown.SelectedIndex = 0;
+            Neighborhood selected = (Neighborhood) NeighborhoodDropdown.SelectedItem;
+            DisplayNeighborhoodImage(selected.Directory);
+        }
+
+        private void DisplayNeighborhoodImage(string sourceDir)
+        {
+            string[] files = Directory.GetFiles(sourceDir);
+            foreach (string file in files)
+            {
+                if (file.EndsWith("Neighborhood.png"))
+                {
+                    NeighborhoodImageBox.ImageLocation = file;
+                    return;
+                }
+            }
+            NeighborhoodImageBox.ImageLocation = null;
+        }
+
+        private System.Windows.Forms.ComboBox NeighborhoodDropdown;
+        private System.Windows.Forms.Button DuplicateButton;
+        private System.Windows.Forms.Panel MainPanel;
+        private System.Windows.Forms.ProgressBar CopyProgressBar;
+        private System.Windows.Forms.PictureBox NeighborhoodImageBox;
+
+        public class Neighborhood
+        {
+            private string myName;
+            private string myDirectory;
+
+            public Neighborhood(string strName, string strDirectory)
+            {
+                this.myName = strName;
+                this.myDirectory = strDirectory;
+            }
+
+            public string Name
+            {
+                get
+                {
+                    return myName;
+                }
+            }
+
+            public string Directory
+            {
+                get
+                {
+                    return myDirectory;
+                }
+            }
         }
 
         private void DuplicateButton_Click(object sender, EventArgs e)
@@ -41,6 +121,12 @@ namespace Sims2HoodDuplicator
             {
                 DuplicateButton.Enabled = true;
             }
+        }
+
+        private void NeighborhoodDropdown_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Neighborhood selected = (Neighborhood)NeighborhoodDropdown.SelectedItem;
+            DisplayNeighborhoodImage(selected.Directory);
         }
     }
 }
