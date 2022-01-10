edb512a97884d6986639b806f2478959533f3ad7
Aaron Ong <icmnfrsh@gmail.com>
2022-01-10 01:24:10 -0600

    WIP: Duplicator assigns unused NIDs to neighborhood package files and renames storytelling files

--- Sims2HoodDuplicator/Duplication.cs
+++ Sims2HoodDuplicator/Duplication.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.IO;
 using System.Windows.Forms;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Text;
 
 namespace Sims2HoodDuplicator
 {
@@ -55,6 +57,7 @@ namespace Sims2HoodDuplicator
                 Directory.CreateDirectory(neighborhoodsDir);
             }
 
+            List<uint> usedNIDs = GetUsedNIDs();
             string newDirectory = Path.Combine(GetUserNeighborhoodsDirectory(), newFolderName);
             string sourceFolderName = Path.GetFileName(sourceDir);
             string destFolderName = Path.GetFileName(newDirectory);
@@ -83,6 +86,8 @@ namespace Sims2HoodDuplicator
             {
                 StorytellingCopy(sourceStorytellingFiles, Path.Combine(newDirectory, "Storytelling"), progressBar);
             }
+
+            EditCopiedNIDs(newDirectory, destFolderName, usedNIDs);
 
             return newFolderName;
         }
@@ -132,6 +137,116 @@ namespace Sims2HoodDuplicator
                         CopiedBytes += file.Length;
                         progressBar?.Invoke(new Action(() => progressBar.Value = (int)(((double)CopiedBytes / TotalBytes) * 100)));
                     }
+                }
+            }
+        }
+
+        private static void EditCopiedNIDs(string newFolder, string destFolderName, List<uint> usedNIDs)
+        {
+            FileInfo[] files = new DirectoryInfo(newFolder).GetFiles();
+            string storytellingDirectory = Path.Combine(newFolder, "Storytelling");
+            foreach (FileInfo file in files)
+            {
+                if (NeighborhoodRegex.IsMatch(file.Name))
+                {
+                    var oldID = GetNeighborhoodNID(file.FullName);
+                    uint newID = 0;
+                    for (uint i = 1; i <= uint.MaxValue; i++)
+                    {
+                        if (usedNIDs.BinarySearch(i) < 0)
+                        {
+                            newID = i;
+                            break;
+                        }
+                    }
+
+                    if (newID == 0) // ran out of possible NIDs for some reason
+                    {
+                        return;
+                    }
+
+                    usedNIDs.Add(newID);
+                    usedNIDs.Sort();
+                    using (FileStream stream = File.Open(file.FullName, FileMode.OpenOrCreate))
+                    {
+                        stream.Seek(oldID.Location, SeekOrigin.Begin);
+                        byte[] data = new byte[4];
+                        stream.Read(data, 0, 4);
+                        uint neighborhoodBytes = BitConverter.ToUInt32(data, 0);
+                        var nameBytes = Encoding.Unicode.GetBytes(destFolderName);
+                        byte[] newNeighborhoodBytes = new byte[4];
+                        int index = 0;
+                        foreach (byte b in nameBytes)
+                        {
+                            if (b != 0)
+                            {
+                                newNeighborhoodBytes[index++] = b;
+                            }
+                        }
+
+                        if (neighborhoodBytes != 4) // Really wish Maxis kept folder names fixed at 4 characters :\
+                        {
+                            string tempFile = Path.GetTempFileName();
+                            using (FileStream temp = File.Open(tempFile, FileMode.Create, FileAccess.ReadWrite))
+                            {
+                                stream.CopyTo(temp);
+                                stream.Seek(oldID.Location, SeekOrigin.Begin);
+                                stream.SetLength(stream.Position);
+                                data = new byte[4] { 0x04, 0x00, 0x00, 0x00 };
+                                stream.Write(data, 0, 4);
+                                stream.Write(newNeighborhoodBytes, 0, 4);
+                                long currentPosition = stream.Position;
+                                temp.Seek(neighborhoodBytes, SeekOrigin.Begin);
+                                temp.CopyTo(stream);
+                                stream.Seek(currentPosition, SeekOrigin.Begin);
+                            }
+                            File.Delete(tempFile);
+                        }
+                        else
+                        {
+                            stream.Write(newNeighborhoodBytes, 0, 4);
+                        }
+                        data[0] = (byte)newID;
+                        data[1] = (byte)(newID >> 8);
+                        data[2] = (byte)(newID >> 16);
+                        data[3] = (byte)(newID >> 24);
+                        stream.Write(data, 0, 4);
+                    }
+
+                    UpdateNeighborhoodStorytelling(storytellingDirectory, oldID.NID, newID);
+                }
+            }
+        }
+
+        private static void UpdateNeighborhoodStorytelling(string storytellingDir, uint oldID, uint newID)
+        {
+            if (!Directory.Exists(storytellingDir))
+            {
+                return;
+            }
+
+            string oldIDHex = oldID.ToString("x8");
+            string newIDHex = newID.ToString("x8");
+            string hyphenatedOldID = string.Format("_{0}_", oldIDHex);
+            string hyphenatedNewID = string.Format("_{0}_", newIDHex);
+            string webentryOldID = string.Format("webentry_{0}", oldIDHex);
+            string webentryNewID = string.Format("webentry_{0}", newIDHex);
+            string oldIDNode = string.Format("<ID>0x{0}</ID>", oldIDHex);
+            string newIDNode = string.Format("<ID>0x{0}</ID>", newIDHex);
+            FileInfo[] screenshots = new DirectoryInfo(storytellingDir).GetFiles();
+            foreach (FileInfo screenshot in screenshots)
+            {
+                if (screenshot.Name.Contains(hyphenatedOldID))
+                {
+                    File.Move(screenshot.FullName, screenshot.FullName.Replace(hyphenatedOldID, hyphenatedNewID));
+                }
+                else if (screenshot.Name.Contains(webentryOldID))
+                {
+                    string xml = File.ReadAllText(screenshot.FullName);
+                    xml = xml.Replace(hyphenatedOldID, hyphenatedNewID);
+                    xml = xml.Replace(oldIDNode, newIDNode);
+                    File.WriteAllText(screenshot.FullName, xml);
+                    File.Move(screenshot.FullName, screenshot.FullName.Replace(webentryOldID, webentryNewID));
                 }
             }
         }
@@ -191,6 +306,102 @@ namespace Sims2HoodDuplicator
             }
         }
 
+        internal static List<uint> GetUsedNIDs()
+        {
+            List<uint> usedNIDs = new List<uint>();
+            DirectoryInfo directoryInfo = Directory.CreateDirectory(GetUserNeighborhoodsDirectory());
+            DirectoryInfo[] userNeighborhoods = directoryInfo.GetDirectories();
+            foreach (DirectoryInfo neighborhood in userNeighborhoods)
+            {
+                FileInfo[] packages = neighborhood.GetFiles();
+                foreach (FileInfo package in packages)
+                {
+                    if (NeighborhoodRegex.IsMatch(package.Name))
+                    {
+                        NeighborhoodID info = GetNeighborhoodNID(package.FullName);
+                        if (info != null)
+                        {
+                            usedNIDs.Add(info.NID);
+                        }
+                    }
+                }
+            }
+
+            usedNIDs.Sort();
+            return usedNIDs;
+        }
+
+        internal static NeighborhoodID GetNeighborhoodNID(string neighborhoodFile)
+        {
+            using (FileStream stream = File.OpenRead(neighborhoodFile))
+            {
+                if (stream.Length < 4)
+                {
+                    return null;
+                }
+
+                byte[] data = new byte[4];
+                stream.Read(data, 0, 4);
+                uint location = 0;
+                bool hasNoLocation = true;
+                int lastRead;
+                do
+                {
+                    if (BitConverter.ToUInt32(data, 0) == 0xac8a7a2e)
+                    {
+                        uint value;
+                        do
+                        {
+                            stream.Read(data, 0, 4);
+                            value = BitConverter.ToUInt32(data, 0);
+                        }
+                        while (value == 0xffffffff || value == 0x00000000 || value == 0x00000001);
+
+                        location = value;
+                        hasNoLocation = false;
+                        break;
+                    }
+
+                    lastRead = stream.ReadByte();
+                    data[0] = data[1];
+                    data[1] = data[2];
+                    data[2] = data[3];
+                    data[3] = (byte) lastRead;
+                }
+                while (lastRead != -1);
+
+                if (hasNoLocation)
+                {
+                    return null;
+                }
+
+                stream.Seek(location + 4, SeekOrigin.Begin);
+                location = ((uint)stream.Position);
+                stream.Read(data, 0, 4);
+                uint neighborhoodBytes = BitConverter.ToUInt32(data, 0);
+                stream.Seek(neighborhoodBytes, SeekOrigin.Current);
+                stream.Read(data, 0, 4);
+                uint NID = BitConverter.ToUInt32(data, 0);
+                return new NeighborhoodID(NID, location);
+            }
+        }
+
+        public class NeighborhoodID
+        {
+            private readonly uint myNID;
+            private readonly uint myLocation;
+
+            public NeighborhoodID(uint NID, uint location)
+            {
+                this.myNID = NID;
+                this.myLocation = location;
+            }
+
+            public uint NID { get { return myNID; } }
+            public uint Location { get { return myLocation; } }
+        }
+
+        private static readonly Regex NeighborhoodRegex = new Regex(@"(Neighborhood|(Suburb|Downtown|University|Vacation)[0-9]{3})\.package");
         private static long CopiedBytes, TotalBytes;
     }
 }
